a1c50fc4508d1af82a289dff431ceeaee527c88c
Stan Alexandru-Stefan <stan.alexandru26@gmail.com>
2022-04-04 16:20:40 +0300

    made the dashboard route protected

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { lazy, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import UserContext from './context/userContext';
 import useAuthListener from './hooks/useAuth';
@@ -13,12 +14,30 @@ const Profile = lazy(() => import('./pages/profile'));
 const NotFound = lazy(() => import('./pages/notFound'));
 
 function App() {
+  const ProtectedRoute = ({ children }) => {
+    const { user } = useAuthListener();
+    const location = useLocation();
+
+    if (!user) {
+      return <Navigate to={ROUTES.LOGIN} replace state={{ from: location }} />;
+    }
+
+    return children;
+  };
+
   const { user } = useAuthListener();
   return (
     <UserContext.Provider value={user}>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+          <Route
+            path={ROUTES.DASHBOARD}
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
           <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.PROFILE} element={<Profile />} />
           <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
@@ -31,3 +50,4 @@ function App() {
 }
 
 export default App;
+
